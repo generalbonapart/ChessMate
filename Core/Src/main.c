89e2d6343c35bc089c088567dde72e4105b3b64b
Alex K <akopylov@sfu.ca>
2024-04-03 23:57:44 -0700

    Modified version of main program. UART receive has to be debugged

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -63,46 +63,40 @@ uint8_t rx_idx = 0;
 uint8_t rx_buffer[10];
 volatile uint8_t transfer_done = 0;
 
-void HAL_UART_RxHalfCpltCallback(UART_HandleTypeDef *huart)
-{
-	HAL_GPIO_TogglePin(LD3_GPIO_Port, LD3_Pin);
-}
-
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
 {
-	HAL_UART_Receive_DMA(&huart2, rx_buffer, 4);
-	HAL_UART_Transmit(&huart2, rx_buffer, 10, 100);
-	/* Prevent unused argument(s) compilation warning
-
-	if (huart->Instance == USART2)
-	{
-		uint8_t i;
-		if (rx_idx == 0)
-		{
-			for (i = 0; i < 100; i++)
-				rx_buffer[i] = 0;
-		}
-		if (rx_data[0] != 13)
-		{
-			rx_buffer[rx_idx++] = rx_data[0];
-		}
-		else
-		{
-			rx_idx = 0;
-			transfer_done = 1;
-			HAL_UART_Transmit(&huart2, "\n\r", 2, 100);
-		}
-		HAL_UART_Receive_IT(&huart2, rx_data, 1);
-		HAL_UART_Transmit(&huart2, rx_data, 2, 100);
-	}
-	*/
+  HAL_UART_Receive_IT(&huart2, rx_buffer, 4);
+  HAL_UART_Transmit(&huart2, rx_buffer, 10, 100);
+  /*
+    if (huart->Instance == USART2)
+    {
+      uint8_t i;
+      if (rx_idx == 0)
+      {
+        for (i = 0; i < 100; i++)
+          rx_buffer[i] = 0;
+      }
+      if (rx_data[0] != 13)
+      {
+        rx_buffer[rx_idx++] = rx_data[0];
+      }
+      else
+      {
+        rx_idx = 0;
+        transfer_done = 1;
+        HAL_UART_Transmit(&huart2, "\n\r", 2, 100);
+      }
+      HAL_UART_Receive_IT(&huart2, rx_data, 1);
+      HAL_UART_Transmit(&huart2, rx_data, 2, 100);
+    }
+    */
 }
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
+ * @brief  The application entry point.
+ * @retval int
+ */
 int main(void)
 {
   /* USER CODE BEGIN 1 */
@@ -130,51 +124,50 @@ int main(void)
   MX_DMA_Init();
   MX_USART2_UART_Init();
   /* USER CODE BEGIN 2 */
-	HAL_UART_Receive_IT(&huart2, rx_buffer, 4);
-	// HAL_UART_Transmit(&huart2, rx_buffer, strlen(rx_buffer), 100);
-	// char *input_move[] = {"d2d4", "b1c3", "a2a4", "g1h3", "f2f3"};
-	// char output_move[4];
+
+  // char *input_move[] = {"d2d4", "b1c3", "a2a4", "g1h3", "f2f3"};
+  char output_move[4];
 
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-	// int i = 0;
-	while (1)
-	{
-		HAL_GPIO_TogglePin(LD3_GPIO_Port, LD3_Pin);
-		//  parse_move(8, (uint8_t *)input_move[i % 5]);
-		//  make_move(16, output_move);
-		// strncpy(tx_buffer, input_move[i % 5], 4);
-		// i++;
-		// HAL_UART_Transmit(&huart2, tx_buffer, strlen(tx_buffer), 100);
-		HAL_Delay(500);
+  while (1)
+  {
+    HAL_UART_Receive(&huart2, rx_buffer, 4, 100);
+    parse_move(8, (uint8_t *)rx_buffer);
+    make_move(16, output_move);
+    strncpy(tx_buffer, output_move, 4);
+    HAL_UART_Transmit(&huart2, tx_buffer, strlen(tx_buffer), 100);
+
+    /* Just to keep track of the program, blink the LED */
+    HAL_GPIO_TogglePin(LD3_GPIO_Port, LD3_Pin);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-	}
+  }
   /* USER CODE END 3 */
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+ * @brief System Clock Configuration
+ * @retval None
+ */
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
   /** Configure the main internal regulator output voltage
-  */
+   */
   if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) != HAL_OK)
   {
     Error_Handler();
   }
 
   /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
+   * in the RCC_OscInitTypeDef structure.
+   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_MSI;
   RCC_OscInitStruct.MSIState = RCC_MSI_ON;
   RCC_OscInitStruct.MSICalibrationValue = 0;
@@ -186,9 +179,8 @@ void SystemClock_Config(void)
   }
 
   /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+   */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_MSI;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
@@ -201,10 +193,10 @@ void SystemClock_Config(void)
 }
 
 /**
-  * @brief USART2 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief USART2 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_USART2_UART_Init(void)
 {
 
@@ -232,12 +224,11 @@ static void MX_USART2_UART_Init(void)
   /* USER CODE BEGIN USART2_Init 2 */
 
   /* USER CODE END USART2_Init 2 */
-
 }
 
 /**
-  * Enable DMA controller clock
-  */
+ * Enable DMA controller clock
+ */
 static void MX_DMA_Init(void)
 {
 
@@ -248,19 +239,18 @@ static void MX_DMA_Init(void)
   /* DMA1_Channel6_IRQn interrupt configuration */
   HAL_NVIC_SetPriority(DMA1_Channel6_IRQn, 0, 0);
   HAL_NVIC_EnableIRQ(DMA1_Channel6_IRQn);
-
 }
 
 /**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief GPIO Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_GPIO_Init(void)
 {
   GPIO_InitTypeDef GPIO_InitStruct = {0};
-/* USER CODE BEGIN MX_GPIO_Init_1 */
-/* USER CODE END MX_GPIO_Init_1 */
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+  /* USER CODE END MX_GPIO_Init_1 */
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOC_CLK_ENABLE();
@@ -277,8 +267,8 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(LD3_GPIO_Port, &GPIO_InitStruct);
 
-/* USER CODE BEGIN MX_GPIO_Init_2 */
-/* USER CODE END MX_GPIO_Init_2 */
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+  /* USER CODE END MX_GPIO_Init_2 */
 }
 
 /* USER CODE BEGIN 4 */
@@ -286,33 +276,33 @@ static void MX_GPIO_Init(void)
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
-	/* User can add his own implementation to report the HAL error return state */
-	__disable_irq();
-	while (1)
-	{
-	}
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
   /* USER CODE END Error_Handler_Debug */
 }
 
-#ifdef  USE_FULL_ASSERT
+#ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
 void assert_failed(uint8_t *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
-	/* User can add his own implementation to report the file name and line number,
-	   ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
   /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
