1149c2e0f79bc56cd78c42b04778fa7bd9344856
Sidd <ssawant0202@gmail.com>
2024-04-24 14:44:41 -0700

    chess UI - Bot auto accepts challenges and all the moves are stored in a txt file

--- chessUI/data_scrapper.py
+++ chessUI/data_scrapper.py
@@ -1,6 +1,13 @@
 import lichess.api
 import berserk
 import time
+import requests
+
+game_id = 'tn5IGFmk'
+file_path = 'moves.txt'
+USER_API_TOKEN = 'lip_HaJoxjLLofX2FRgDJlBD'
+turn = 'white'
+prev_size = 0
 
 
 
@@ -17,31 +24,34 @@ def save_moves_to_file(moves, file_path):
 # Function to create a new game with a bot
 def create_new_game_with_bot():
     # Parameters for creating a new game
-    parameters = {
-        "rated": True,  # Set to True for a rated game, False for an unrated game
-        "variant": "standard",  # Chess variant (standard, chess960, etc.)
-        "timeControl": "180+2",  # Time control in seconds (180 seconds initial time + 2 seconds increment)
-        "color": "random",  # Choose color randomly (can also be "white" or "black")
-        "level": 3  # Bot level (ranges from 1 to 8, with 1 being the easiest and 8 being the hardest)
-    }
-
-    #response = client.bots.
-    #game_id = response['challenge']['id']
+    username = "automatechessmate"
+    rated = False  # Set to True for a rated game, False for an unrated game
+    clock_limit = 3
+    clock_increment = 3
+    days = 0
+    color = "random"  # Choose color randomly (can also be "white" or "black")
+    variant = "standard"  # Chess variant (standard, chess960, etc.)
+    token = "lip_OOb8ZjPb0XzdGP8tL6Zz" # bots token
+    level= 3  # Bot level (ranges from 1 to 8, with 1 being the easiest and 8 being the hardest)
+    response = client.challenges.create_with_accept(username=username, rated=rated, token=token, clock_limit=180, clock_increment=20, days = None, color = 'white', variant=variant, position=None)
+    game_id = response['challenge']['id']
     return game_id
 
 
 if __name__ == "__main__":
-    game_id = 'tn5IGFmk'
-    file_path = 'moves.txt'
-    API_TOKEN = 'lip_HaJoxjLLofX2FRgDJlBD'
-    session = berserk.TokenSession(API_TOKEN)
+    session = berserk.TokenSession(USER_API_TOKEN)
     client = berserk.Client(session=session)
     game_id = create_new_game_with_bot()
-
     print(game_id)
+    time.sleep(2)
+    moves = ['g1f3', 'g2g3', 'f1g2', 'e1g1', 'd2d3', 'b1d2', 'd1e1' ]
+    #'e2e4', 'd2d3', 
 
-    while False:
+    for move in moves:
+        time.sleep(1)
+        response = client.board.make_move(game_id, move)
         moves = get_game_moves(game_id)
         save_moves_to_file(moves, file_path)
         print("Moves updated.")
-        time.sleep(2)  # Wait for 60 seconds before fetching moves again
+        time.sleep(25)  # Wait for 60 seconds before fetching moves again
+    client.board.resign_game(game_id)
