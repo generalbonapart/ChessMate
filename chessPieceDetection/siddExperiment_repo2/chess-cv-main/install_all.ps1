8bf3a1b1fc5a74d5ff581bf54cb784d427f26883
Sidd <ssawant0202@gmail.com>
2024-05-24 08:37:41 -0700

    Added folder for site deployment

---
+++ chessPieceDetection/siddExperiment_repo2/chess-cv-main/install_all.ps1
@@ -0,0 +1,14 @@
+
+Write-Host "Installing all dependencies"
+cd "E:\ChessMate\chessPieceDetection\siddExperiment_repo2\chess-cv-main"
+
+pip install keras  
+pip install -U matplotlib  
+pip install numpy  
+pip install opencv-contrib-python  
+pip install chess  
+pip install scipy  
+pip install tensorflow  
+pip install wand
+
+Write-Host "Done installing!"
