66f5f1fccfe0bbe1615738768cbf40f738ad866a
Sidd <ssawant0202@gmail.com>
2024-05-30 01:11:10 -0700

    Added a new file with less if and else

---
+++ chessPieceMovement/test.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf("Hello, World!\n");
+    // fflush(stdout); // Ensure the output is flushed to the console
+    system("pause");
+    return 0;
+}
