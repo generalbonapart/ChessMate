78e0e73d71f2a7e8b45997cbbcc569ece6111300
Alex K <akopylov@sfu.ca>
2024-06-03 20:57:19 -0700

    Created a final product environment + flask server

---
+++ chessApp/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+from lichess_api import launch_game
+from models import GameParams
+import time
+
+def handle_game_start(request):
+    params = GameParams()
+    params.side = request.form['color']
+    params.time = int(request.form['time_limit']) * 60 # time in seconds
+    params.time_inc = int(request.form['time_increment'])
+    params.level = int(request.form['difficulty'])
+    
+    launch_game(params)
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        handle_game_start(request)
+
+    colors = ['white', 'black']
+    difficulties = list(range(1, 9))
+
+    return render_template('index.html', colors=colors, difficulties=difficulties)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
